f3f213398b76cdeed663deca84118dc5a283c18e
PiotrSzczepanski8 <114732037+PiotrSzczepanski8@users.noreply.github.com>
2024-11-24 19:38:36 +0100

    home page

---
+++ website/client/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/style.css">
+    <link rel="shortcut icon" href="../public/logo.svg" type="image/x-icon">
+    <title>Zaloguj się</title>
+</head>
+<body>
+    <div class="container">
+        <?php
+            include "../public/header.shtml";
+        ?>
+        <main>
+            nic tutaj nie ma...
+        </main>
+    </div>
+</body>
+</html>
