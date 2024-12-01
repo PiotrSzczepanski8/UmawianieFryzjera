e574d77ec2df2a8161ac4c31ae00bb8ccdd17719
PiotrSzczepanski8 <114732037+PiotrSzczepanski8@users.noreply.github.com>
2024-12-01 19:27:41 +0100

    update: database data

--- zaklad_fryzjerski.sql
+++ zaklad_fryzjerski.sql
@@ -1,7 +1,9 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
+--
 -- Host: 127.0.0.1
+-- Czas generowania: 01 Gru 2024, 19:26
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 8.1.6
 
@@ -33,6 +35,15 @@ CREATE TABLE `fryzjer` (
   `nazwisko` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `fryzjer`
+--
+
+INSERT INTO `fryzjer` (`id_fryzjer`, `imie`, `nazwisko`) VALUES
+(1, 'Marek', 'Rurka'),
+(2, 'Weronika', 'Krzywy'),
+(3, 'Maria', 'Janosik');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +58,58 @@ CREATE TABLE `rezerwacja` (
   `id_fryzjer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `rezerwacja`
+--
+
+INSERT INTO `rezerwacja` (`id_rezerwacja`, `data`, `godzina`, `id_uzytkownik`, `id_fryzjer`) VALUES
+(3, '2024-12-04', '10:30:00', 2147483647, 2),
+(4, '2024-12-04', '10:30:00', 2147483647, 2),
+(5, '2024-12-04', '10:30:00', 2147483647, 2),
+(6, '2024-12-04', '09:45:00', 2147483647, 3),
+(7, '2024-12-04', '09:45:00', 2147483647, 1),
+(8, '2024-12-04', '12:00:00', 2147483647, 2),
+(9, '2024-12-04', '13:30:00', 2147483647, 1),
+(10, '2024-12-04', '15:00:00', 2147483647, 3),
+(11, '2024-12-04', '16:30:00', 2147483647, 1),
+(12, '2024-12-04', '15:00:00', 2147483647, 2),
+(13, '2024-12-04', '17:15:00', 2147483647, 3),
+(14, '2024-12-04', '17:15:00', 2147483647, 2),
+(15, '2024-12-03', '10:30:00', 2147483647, 1),
+(16, '2024-12-03', '10:30:00', 2147483647, 2),
+(17, '2024-12-03', '09:45:00', 2147483647, 3),
+(18, '2024-12-05', '09:45:00', 2147483647, 2),
+(19, '2024-12-05', '10:30:00', 2147483647, 2),
+(20, '2024-12-05', '11:15:00', 2147483647, 2),
+(21, '2024-12-05', '12:00:00', 2147483647, 2),
+(22, '2024-12-05', '12:00:00', 2147483647, 3),
+(23, '2024-12-05', '11:15:00', 2147483647, 3),
+(24, '2024-12-06', '09:45:00', 2147483647, 1),
+(25, '2024-12-06', '10:30:00', 2147483647, 1),
+(26, '2024-12-06', '11:15:00', 2147483647, 1),
+(27, '2024-12-09', '09:45:00', 2147483647, 2),
+(28, '2024-12-09', '10:30:00', 2147483647, 2),
+(29, '2024-12-09', '10:30:00', 2147483647, 3),
+(30, '2024-12-10', '09:45:00', 2147483647, 3),
+(31, '2024-12-10', '10:30:00', 2147483647, 3),
+(32, '2024-12-10', '11:15:00', 2147483647, 3),
+(33, '2024-12-10', '12:00:00', 2147483647, 3),
+(34, '2024-12-10', '10:30:00', 2147483647, 1),
+(35, '2024-12-10', '11:15:00', 2147483647, 1),
+(36, '2024-12-10', '12:00:00', 2147483647, 1),
+(37, '2024-12-10', '11:15:00', 2147483647, 2),
+(39, '2024-12-02', '09:45:00', 2147483647, 1),
+(40, '2024-12-02', '10:30:00', 2147483647, 1),
+(41, '2024-12-02', '11:15:00', 2147483647, 2),
+(42, '2024-12-02', '10:30:00', 2147483647, 2),
+(43, '2024-12-02', '15:00:00', 2147483647, 2),
+(44, '2024-12-02', '16:30:00', 2147483647, 2),
+(45, '2024-12-02', '17:15:00', 2147483647, 2),
+(46, '2024-12-02', '15:45:00', 2147483647, 2),
+(47, '2024-12-02', '12:00:00', 2147483647, 3),
+(48, '2024-12-02', '11:15:00', 2147483647, 3),
+(49, '2024-12-02', '09:45:00', 2147483647, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -57,10 +120,17 @@ CREATE TABLE `uzytkownik` (
   `PESEL` int(11) NOT NULL,
   `imie` varchar(30) NOT NULL,
   `nazwisko` varchar(30) NOT NULL,
-  `telefon` varchar(15) NOT NULL,
   `login` varchar(30) NOT NULL,
-  `haslo` varchar(100) NOT NULL
+  `haslo` varchar(100) NOT NULL,
+  `telefon` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `uzytkownik`
+--
+
+INSERT INTO `uzytkownik` (`PESEL`, `imie`, `nazwisko`, `login`, `haslo`, `telefon`) VALUES
+(2147483647, 'Karolina', 'Fryderyk', 'marekrurka', 'kurka', '+90 000 000 000');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -94,13 +164,13 @@ ALTER TABLE `uzytkownik`
 -- AUTO_INCREMENT dla tabeli `fryzjer`
 --
 ALTER TABLE `fryzjer`
-  MODIFY `id_fryzjer` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_fryzjer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT dla tabeli `rezerwacja`
 --
 ALTER TABLE `rezerwacja`
-  MODIFY `id_rezerwacja` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_rezerwacja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- Ograniczenia dla zrzutów tabel
